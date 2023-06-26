c05d10aec20b54a798a2c29ea1820c21d429d4bc
exxon suarez <suarez.exxon@gmail.com>
2023-06-26 13:43:45 -0700

    created vite react project

---
+++ src/components/TestComp.jsx
@@ -0,0 +1,12 @@
+function TestComp() {
+
+    return (
+        <>
+            <div>
+                <h1> This should be inserted into the default page??!! </h1>
+            </div>
+        </>
+    )
+}
+
+export default TestComp
