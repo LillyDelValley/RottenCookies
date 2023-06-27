cc4876a525ebce0af90f7abcff25ad1109e2a5b7
Lilly <brittany.lilly@TLGcohort.com>
2023-06-27 09:13:21 -0800

    attempting to add bootstrap to header. Undesireable results, sending to Ex

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/Header'
 import TopShowCarousel from './components/TopShowCarousel.jsx'
 import './css/index.css'
