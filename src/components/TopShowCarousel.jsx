b903315b999126217803f68eb1fd9ed0fa825627
Lilly <brittany.lilly@TLGcohort.com>
2023-06-27 08:31:14 -0800

    Intial playground

---
+++ src/components/TopShowCarousel.jsx
@@ -0,0 +1,13 @@
+
+function TopShowCarousel() {
+
+    return (
+        <div>
+            <div>
+            <h1>this will be a carousel</h1>
+            </div>
+        </div>
+    )
+}
+
+export default TopShowCarousel
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
 import Header from './components/Header'
+import TopShowCarousel from './components/TopShowCarousel.jsx'
 import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
-    <App />
+    <TopShowCarousel />
   </React.StrictMode>,
 )
